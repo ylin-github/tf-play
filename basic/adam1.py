16757e4bb8a46c915ff6b698bd68389d9af20ec5
ylin-github <yuan@admins-Mac-mini.local>
2018-09-09 21:00:20 -0700

    update

--- basic/adam1.py
+++ basic/adam1.py
@@ -4,11 +4,11 @@ x = tf.Variable(10.0, name="x")
 y = tf.Variable(50.0, name="y")
 cost = tf.add(tf.square(x), tf.square(y), name="cost")
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
 train_op = optimizer.minimize(cost)
 
 with tf.Session() as sess:
-  with tf.summary.FileWriter('gd1', sess.graph) as sum:
+  with tf.summary.FileWriter('adam1', sess.graph) as sum:
     sess.run(tf.global_variables_initializer())
     print sess.run(cost)
 
